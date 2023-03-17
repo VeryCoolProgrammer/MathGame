885df3afb50d34e5e7defabc9868fc0c3f120760
java2.1 <eta19777@gmail.com>
2023-03-17 19:51:22 +0300

    6-7

--- core/src/com/mygdx/game/entities/Player.java
+++ core/src/com/mygdx/game/entities/Player.java
@@ -6,19 +6,28 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.mygdx.game.MyGdxGame;
+import com.mygdx.game.handlers.Animation;
+import com.mygdx.game.handlers.MyInputProcessor;
 
 import static com.mygdx.game.handlers.B2DVars.*;
 
-public class Player extends B2DSprite{
+public class Player extends B2DSprite {
+    private TextureRegion[] sprites;
+    private Texture tex;
+
+    private MyInputProcessor mip;
+
     public Player(Body body) {
         super(body);
-        Texture tex = MyGdxGame.res.getTexture("gnomik");
-        TextureRegion[] sprites = TextureRegion.split(tex, 110,130)[0]; //110 130 - 1row, 120 130 - step
+        tex = MyGdxGame.res.getTexture("gnomik");
+        sprites = TextureRegion.split(tex, 110, 130)[0]; //110 130 - 1row, 120 130 - step
 
         speed = 40f;
-        setAnimation(sprites, 1/12f);
+        animUpdate(); // ?????
+        //setAnimation(sprites, 1 / 12f);
     }
-    public void updatePL(){
+
+    public void updatePL() {
         x = body.getPosition().x * PPM;
         y = body.getPosition().y * PPM;
         checkUserInput();
@@ -27,19 +36,25 @@ public class Player extends B2DSprite{
     private void checkUserInput() {
         velx = 0;
         vely = 0;
-        if(Gdx.input.isKeyPressed(Input.Keys.D)){
+        if (Gdx.input.isKeyPressed(Input.Keys.D)) {
             velx = 1;
         }
-        if(Gdx.input.isKeyPressed(Input.Keys.A)){
+        if (Gdx.input.isKeyPressed(Input.Keys.A)) {
             velx = -1;
         }
-        if(Gdx.input.isKeyPressed(Input.Keys.W)){
+        if (Gdx.input.isKeyPressed(Input.Keys.W)) {
             vely = 1;
         }
-        if(Gdx.input.isKeyPressed(Input.Keys.S)){
+        if (Gdx.input.isKeyPressed(Input.Keys.S)) {
             vely = -1;
         }
-        body.setLinearVelocity(velx*speed, vely*speed);
+        body.setLinearVelocity(velx * speed, vely * speed);
+    }
+    private void animUpdate(){
+        //через myInpProccesor???
+        System.out.println("true");
+        setAnimation(sprites, 1 / 12f);
+        sprites = TextureRegion.split(tex, 150, 130)[0];
     }
 }
 
