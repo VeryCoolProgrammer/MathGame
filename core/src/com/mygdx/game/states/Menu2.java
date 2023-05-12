5a17ba05d016e34c662ee5614416385ffd60b6a7
java2.1 <eta19777@gmail.com>
2023-05-12 19:41:44 +0300

    npc, initFight

---
+++ core/src/com/mygdx/game/states/Menu2.java
@@ -0,0 +1,29 @@
+package com.mygdx.game.states;
+
+import com.mygdx.game.handlers.GameStateManager;
+
+public class Menu2 extends GameState{
+    public Menu2(GameStateManager gsm) {
+        super(gsm);
+    }
+
+    @Override
+    public void handleInput() {
+
+    }
+
+    @Override
+    public void update(float dt) {
+
+    }
+
+    @Override
+    public void render() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
